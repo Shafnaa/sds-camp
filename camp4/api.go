37d6988cd07ecb1f734c8706ebdf8d2f97ffc139
Shafnaa <saujanashafi2003@gmail.com>
2023-09-30 23:46:00 +0700

    SPOILER ALLERTgit add .!

--- camp4/api.go
+++ camp4/api.go
@@ -6,18 +6,11 @@ import (
 
 type RequestData struct {
 	JariJariLingkaran int 	    `json:"jari-jari-lingkaran"`
-	SisiPersegi       int       `json:"sisi-persegi"`
-	AlasSegitiga      int       `json:"alas-segitiga"`
-	TinggiSegitiga    int       `json:"tinggi-segitiga"`
 }
 
 type ResponseData struct {
 	LuasLingkaran 		float64     `json:"luas-lingkaran"`
-	LuasPersegi 		int 		`json:"luas-persegi"`
-	LuasSegitiga 		float64 	`json:"luas-segitiga"`
 	KelilingLingkaran   float64		`json:"keliling-lingkaran"`
-	KelilingPersegi 	int 		`json:"keliling-persegi"`
-	KelilingSegitiga 	int 		`json:"keliling-segitiga"`
 }
 
 func main() {
@@ -33,11 +26,7 @@ func main() {
 		response := new(ResponseData)
 
 		response.LuasLingkaran = 3.14 * float64(request.JariJariLingkaran * request.JariJariLingkaran)
-		response.LuasPersegi = request.SisiPersegi * request.SisiPersegi 
-		response.LuasSegitiga = float64(request.AlasSegitiga * request.TinggiSegitiga) / 2
 		response.KelilingLingkaran = 2 * float64(request.JariJariLingkaran) * 3.14
-		response.KelilingPersegi = 4 * request.SisiPersegi
-		response.KelilingSegitiga = 3 * request.AlasSegitiga
 
 		return c.JSON(response) 
 	})
